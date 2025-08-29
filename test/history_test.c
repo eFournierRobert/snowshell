9910d947aee3fa0c4f882983aaa56dd0bbd16e48
efournierrobert <elliott1447@gmail.com>
2025-08-29 15:21:53 -0400

    get_commands_history tests

--- test/history_test.c
+++ test/history_test.c
@@ -1,8 +1,10 @@
 #include <criterion/criterion.h>
 #include <criterion/internal/assert.h>
 #include <limits.h>
+#include <linux/limits.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 
 // Just for the LSP
@@ -18,6 +20,24 @@ void setup(void) {
     getcwd(buf, sizeof(buf));
 
     setenv("HOME", buf, 1);
+}
+
+void full_history_file_setup(void) {
+    // Change HOME to the current directory as to not ruin the current
+    // tester homedir.
+    setup();
+
+    char path[PATH_MAX];
+    snprintf(path, sizeof(path), "%s/.snowshell_history", getenv("HOME"));
+
+    FILE *fptr;
+    fptr = fopen(path, "w");    
+
+    //Dummy history
+    for (int i = 0; i < 5; i++)
+        fprintf(fptr, "Command: %d\n", i);
+
+    fclose(fptr);
 }
 
 void teardown(void) {
@@ -50,4 +70,34 @@ Test (history_test_suite, get_hist_file_readptr_when_existing, .init=setup, .fin
 
     cr_assert_not_null(fptr, "Returned file pointer was null.");
     fclose(fptr);
+}
+
+Test(history_test_suite, get_command_history_working, .init=full_history_file_setup, .fini=teardown) {
+    history_t dest_hist;
+    dest_hist.length = 0;
+    char history[MAX_HIST_SIZE][MAX_INPUT];
+    memcpy(dest_hist.hist, history, MAX_HIST_SIZE);
+
+    get_commands_history(&dest_hist);
+
+    for (int i = 0; i < 5; i++) {
+        char str[25];
+        snprintf(str, 25, "Command: %d", i);
+
+        cr_assert_str_eq(dest_hist.hist[i], str, "Command history doesn't match.\nExpected: %s\nGot: %s", str, dest_hist.hist[i]);
+    }
+
+    cr_assert_eq(dest_hist.length, 5, "History length doesn't match.\nExpected: %d\nGot: %d", 5, dest_hist.length);
+}
+
+Test(history_test_suite, get_command_history_from_empty_history_file, .init=setup, .fini=teardown) {
+    history_t dest_hist;
+    dest_hist.length = 0;
+    char history[MAX_HIST_SIZE][MAX_INPUT];
+    memcpy(dest_hist.hist, history, MAX_HIST_SIZE);
+
+    get_commands_history(&dest_hist);
+
+    cr_assert_str_empty(dest_hist.hist[0], "First string of history is not empty when it is supposed to be.");
+    cr_assert_eq(dest_hist.length, 0, "History length doesn't match.\nExpected: %d\nGot: %d", 5, dest_hist.length);
 }
